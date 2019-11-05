a3842a565883c2cae0c87b1e6179c7d305258778
Швецов Сергей Владимирович <shvetsov@elvees.com>
2019-11-05 18:54:41 +0300

    added some features

--- p_library/admin.py
+++ p_library/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
+from p_library.models import Author, Book
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_filter = ('country',)
+    pass
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_filter = ('author',)
+    list_display = ('ISBN', 'title', 'author')
+    # fields = ('ISBN', 'title', 'description', 'year_release', 'author', 'price')
+
+
+
 # Register your models here.
