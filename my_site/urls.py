95a0cb3631b88b1524bd8cd8e4adc2c405b699e7
Shvd <2100992@gmail.com>
2019-11-12 01:26:41 +0300

    added some features

--- my_site/urls.py
+++ my_site/urls.py
@@ -14,17 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from p_library import views
-from django.urls import include
+from django.urls import path, include
+from p_library.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index),
-    path('books/', views.books),
-    path('authors/', views.authors),
-    path('publishers/', views.publisher),
-    path('index/book_increment/', views.book_increment),
-    path('index/book_decrement/', views.book_decrement),
+    path('', index),
     path('library/', include('p_library.urls')),
 ]
--- p_library/urls.py
+++ p_library/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
-from django.contrib import admin
-from .views import AuthorEdit, AuthorList
+from . import views
 # from my_site.urls import urlpatterns
 
 app_name = 'p_library'
 
 urlpatterns = [
-    path('author/create/', AuthorEdit.as_view(), name='author_create'),
-    path('authors/', AuthorList.as_view(), name='author_list')
+    path('', views.library),
+    path('books/', views.books, name='books'),
+    path('books/book_increment/', views.book_increment),
+    path('books/book_decrement/', views.book_decrement),
+    path('authors/', views.authors, name='authors'),
+    path('author/create/', views.AuthorEdit.as_view(), name='author_create'),
+    path('author/create_many/', views.author_create_many, name='author_create_many'),
+    path('publishers/', views.publisher),
+    path('f_authors/', views.AuthorList.as_view(), name='author_list'),
 ]
