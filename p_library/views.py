13f29e2bd87a6d0c554080f3efff92198c10a196
Shvd <2100992@gmail.com>
2019-11-12 01:26:11 +0300

    added some features

--- p_library/views.py
+++ p_library/views.py
@@ -1,3 +1,4 @@
+from sys import prefix
 from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse
 from p_library.models import Book, Author, Publisher
@@ -5,12 +6,21 @@ from django.template import loader
 from p_library.forms import AuthorForm
 from django.views.generic import CreateView, ListView
 from django.urls import reverse_lazy
+from django.forms import formset_factory
+from django.http.response import HttpResponseRedirect
 
 # Create your views here.
 
 
 def index(request):
-    template = loader.get_template('index.html')
+    template = loader.get_template('my_site/index.html')
+    data = {
+        "title": 'Мой проект',
+    }
+    return HttpResponse(template.render(data, request))
+
+def library(request):
+    template = loader.get_template('p_library/index.html')
     # books_count = Book.objects.all().count()
     books = Book.objects.all()
     biblio_data = {
@@ -21,7 +31,7 @@ def index(request):
 
 
 def books(request):
-    template = loader.get_template('books.html')
+    template = loader.get_template('p_library/books.html')
     # books_count = Book.objects.all().count()
     books = Book.objects.all()
     biblio_data = {
@@ -35,38 +45,38 @@ def book_increment(request):
     if request.method == 'POST':
         book_id = request.POST['id']
         if not book_id:
-            return redirect('/books/')
+            return redirect('/library/books/')
         else:
             book = Book.objects.filter(id=book_id).first()
             if not book:
-                return redirect('/books/')
+                return redirect('/library/books/')
             book.copy_count += 1
             book.save()
-        return redirect('/books/')
+        return redirect('/library/books/')
     else:
-        return redirect('/books/')
+        return redirect('/library/books/')
 
 
 def book_decrement(request):
     if request.method == 'POST':
         book_id = request.POST['id']
         if not book_id:
-            return redirect('/books/')
+            return redirect('/library/books/')
         else:
             book = Book.objects.filter(id=book_id).first()
             if not book:
-                return redirect('/books/')
+                return redirect('/library/books/')
             if book.copy_count < 1:
                 book.copy_count = 0
             else:
                 book.copy_count -= 1
             book.save()
-        return redirect('/books/')
+        return redirect('/library/books/')
     else:
         return redirect('/books/')
 
 def authors(request):
-    template = loader.get_template('authors.html')
+    template = loader.get_template('p_library/authors.html')
     authors = Author.objects.all()
 
     for author in authors:
@@ -79,7 +89,7 @@ def authors(request):
     return HttpResponse(template.render(authors_data, request))
 
 def publisher(request):
-    template = loader.get_template('publishers.html')
+    template = loader.get_template('p_library/publishers.html')
     # books_count = Book.objects.all().count()
     publishers = Publisher.objects.all() #.prefetch_related('books')
 
@@ -96,12 +106,26 @@ class AuthorEdit(CreateView):
     model = Author
     form_class = AuthorForm
     # success_url = '/library/authors/'
-    success_url = reverse_lazy('p_library:author_list')
-    template_name = 'a_edit.html'
+    success_url = reverse_lazy('p_library:authors')
+    template_name = 'p_library/a_edit.html'
 
 class AuthorList(ListView):
     model = Author
-    template_name = 'a_list.html'
+    template_name = 'p_library/a_list.html'
 
     # def __init__(self, *args, **kwargs):
     #     print(self)
+
+
+
+def author_create_many(request):
+    AuthorFormSet = formset_factory(AuthorForm, extra=2)
+    if request.method == 'POST':
+        author_formset = AuthorFormSet(request.POST, request.FILES, prefix='authors')
+        if author_formset.is_valid():
+            for author_form in author_formset:
+                author_form.save()
+            return HttpResponseRedirect(reverse_lazy('p_library:authors'))
+    else:
+        author_formset = AuthorFormSet(prefix='authors')
+    return render(request, 'p_library/authors_manage.html', {'author_formset':author_formset})
