6a1b9afdc5c4638c2fff47a0d10448db6954892c
Shvd <2100992@gmail.com>
2019-11-13 01:35:20 +0300

    added new models

--- p_library/admin.py
+++ p_library/admin.py
@@ -1,6 +1,7 @@
+from codecs import register
 from django.contrib import admin
 
-from p_library.models import Author, Book, Publisher
+from p_library.models import Author, Book, BooksCopy, LibraryUser, Publisher
 
 
 @admin.register(Author)
@@ -11,7 +12,7 @@ class AuthorAdmin(admin.ModelAdmin):
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_filter = ('author',)
-    list_display = ('ISBN', 'title', 'author', 'publisher')
+    list_display = ('ISBN', 'title', 'publisher')
     # fields = ('ISBN', 'title', 'description', 'year_release', 'author', 'price')
 
 @admin.register(Publisher)
@@ -19,5 +20,13 @@ class AuthorAdmin(admin.ModelAdmin):
     list_filter = ('name',)
     pass
 
+@admin.register(LibraryUser)
+class LibraryUserUserAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(BooksCopy)
+class BooksCopyAdmin(admin.ModelAdmin):
+    list_display = ('book', 'holder')
+    pass
 
 # Register your models here.
--- p_library/models.py
+++ p_library/models.py
@@ -4,17 +4,20 @@ from django.db import models
 
 
 class Author(models.Model):
-    full_name = models.TextField()
+    full_name = models.CharField(max_length=150, db_index=True)
+    slug = models.SlugField(max_length=150, unique=True)
     birth_year = models.SmallIntegerField()
     country = models.CharField(max_length=2)
+    description = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.full_name
 
 
 class Publisher(models.Model):
-    name = models.TextField()
-    description = models.TextField()
+    name = models.CharField(max_length=150, db_index=True)
+    slug = models.SlugField(max_length=150, unique=True)
+    description = models.TextField(null=True, blank=True)
     country = models.CharField(max_length=2)
 
     def __str__(self):
@@ -22,19 +25,14 @@ class Publisher(models.Model):
 
 
 class Book(models.Model):
-    ISBN = models.CharField(max_length=13)
-    title = models.TextField()
-    description = models.TextField()
+    ISBN = models.CharField(max_length=13, unique=True)
+    title = models.CharField(max_length=150, db_index=True)
+    slug = models.SlugField(max_length=150, unique=True)
+    description = models.TextField(null=True, blank=True)
     year_release = models.SmallIntegerField()
     copy_count = models.SmallIntegerField(default=1)
     price = models.DecimalField(max_digits=8, decimal_places=2)
-    author = models.ForeignKey(
-        Author,
-        on_delete=models.CASCADE,
-        null=True,
-        blank=True,
-        related_name='books',
-    )
+    author = models.ManyToManyField(Author)
     publisher = models.ForeignKey(
         Publisher,
         on_delete=models.DO_NOTHING,
@@ -47,21 +45,33 @@ class Book(models.Model):
         return self.title
 
 
-class Subscriber(models.Model):
+class LibraryUser(models.Model):
+    # Таблица пользователей библиотеки
     full_name = models.TextField()
-    library_card = models.CharField(max_length=13)
+    library_card = models.CharField(max_length=13, null=True, blank=True)
 
     def __str__(self):
         return self.full_name
 
 
 class BooksCopy(models.Model):
+    # В этой таблице будем хранить информацию о экземплярах книг
+    # в каком они состоянии
+    # у кого находятся (если ни у кого, значит в библиотеке)
     book = models.ForeignKey(
         Book,
-        on_delete=models.CASCADE,
-        null=True,
-        blank=True,
+        on_delete=models.DO_NOTHING,
         related_name='books_copy',
     )
-    
-    notes = models.TextField()
+    holder = models.ForeignKey(
+        LibraryUser,
+        on_delete=models.DO_NOTHING,
+        null=True,
+        blank=True,
+        related_name='books_copy'
+    )
+    notes = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.book.title
+
