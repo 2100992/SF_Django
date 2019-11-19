1ad3f315d9785affd0c3dc3557ac111e26f680e3
Швецов Сергей Владимирович <shvetsov@elvees.com>
2019-11-19 16:18:10 +0300

    added django-heroku

--- my_site/settings.py
+++ my_site/settings.py
@@ -14,6 +14,9 @@ import os
 import dj_database_url
 from django.utils.crypto import get_random_string
 
+import django_heroku
+django_heroku.settings(locals())
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
