01ca2d165691dad71688977f84540c56f7edd08d
Швецов Сергей Владимирович <shvetsov@elvees.com>
2019-11-19 15:29:03 +0300

    test deploy

--- my_site/settings.py
+++ my_site/settings.py
@@ -87,6 +87,14 @@ WSGI_APPLICATION = 'my_site.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+'''
 try:
     DATABASES = {'default': dj_database_url.config(
         default=os.environ.get('DATABASE_URL'))}
@@ -97,8 +105,15 @@ except:
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
-
-
+'''
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+'''
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
