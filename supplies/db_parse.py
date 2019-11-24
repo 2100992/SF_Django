76ab214fe992b5604f88277afb463e87aef0cbee
Shvd <2100992@gmail.com>
2019-11-24 23:55:58 +0300

    small release

--- supplies/db_parse.py
+++ supplies/db_parse.py
@@ -1,59 +1,103 @@
 import pandas as pd
+from pandas import DataFrame
 
 from collections import Counter
 
-# Читаем .xlsx файл в DataFrame
-df = pd.read_excel('bd.xlsx')
 
-# Заменим все NaN в данных на нули
-df = df.fillna(0)
+def db_parse(df, companies, periods, products):
+    # print(df)
+    # print(companies)
+    # print(periods)
+    # print(products)
 
-# Получим массив компаний
-companies = list(Counter(df.company).keys())
+# result_df = DataFrame({'product':[], 'company':[], 'count':[], 'period':[]})
+    results_list = []
 
-# Предварительный массив продуктов
-products = list(df.columns[2:-9])
+    for product in products:
+        for company in companies:
+            for period in periods:
+                result_series = df[(
+                    df.delivery_date >= periods[period][0]) & (
+                    df.delivery_date <= periods[period][1]) & (
+                    df.company == company)
+                ][products[product]].sum()
 
-year = '2018'
+                if result_series.values.any():
+                    results_list.append([product, company, result_series.sum(), period])
+                    # print(
+                    #     f'{product} - {company} - {result_series.sum()} - {period}')
+    
+    result = DataFrame(results_list)
 
-periods = {
-    f'1кв. {year}г.': [f'{year}-01-01', f'{year}-03-31'],
-    f'2кв. {year}г.': [f'{year}-04-01', f'{year}-06-30'],
-    f'3кв. {year}г.': [f'{year}-07-01', f'{year}-09-30'],
-    f'4кв. {year}г.': [f'{year}-10-01', f'{year}-12-31'],
-}
+    result.columns = [
+        'product',
+        'company',
+        'count',
+        'period',
+    ]
 
-print(periods)
+    print(result)
+
+    result.to_excel('test1.xlsx', sheet_name='sheet1', index=False)
 
 
-# periods = ['2008-01-01', '2008-04-01', '2008-07-01', '2008-10-01','2009-01-01']
+def init():
+
+    # Читаем .xlsx файл в DataFrame
+    # df = pd.read_excel('bd.xlsx')
+    df = pd.read_excel('bd.xlsx')
+
+    # Заменим все NaN в данных на нули
+    df = df.fillna(0)
+
+    # Получим массив компаний
+    companies = list(Counter(df.company).keys())
+    # print(companies)
+
+    # Предварительный массив продуктов из файла
+    # Подсмотрим и распределим по группам
+
+    # products = list(df.columns[1:-1])
+    # print(products)
+
+    # Продукты по группам
+    # ToDo вынести в конфигурационный файл
+    products = {
+        '1892ВМ15Ф': ['1892ВМ15АФ', '1892ВМ15АФ(ОТК)', '1892ВМ15АФ(ВП)'],
+        '1892BM12Т': ['1892ВМ12АТ', '1892ВМ12АТ (ГК)', '1892BM12Т(ФК НУ)', '1892BM12Т(ОТК)', '1892BM12АТ(ВП)'],
+        '1892ВМ2Я': ['1892ВМ2Т\n("ФК")', '1892ВМ2Я\n(ВП)', '1892ВМ2Т\n(ОТК)', '1892ВМ2Я\n(ОТК)', '1892ВМ2Я\n("ФК")'],
+        '1892ВМ10Я': ['1892ВМ10Я\n(ГК)', '1892ВМ10Я(ФК в НУ)', '1892ВМ10Я\n(ОТК)', '1892ВМ10Я\n(ВП)'],
+    }
+
+    # Отчетный год
+    year = '2019'
+
+    quarters = {
+        f'1кв. {year}г.': [f'{year}-01-01', f'{year}-03-31'],
+        f'2кв. {year}г.': [f'{year}-04-01', f'{year}-06-30'],
+        f'3кв. {year}г.': [f'{year}-07-01', f'{year}-09-30'],
+        f'4кв. {year}г.': [f'{year}-10-01', f'{year}-12-31'],
+    }
+
+    # print(quarters)
+
+    # Подготовка группы товаров, содержащих в prod
+    # prod = 'ча'
+    # product = list(filter(lambda x: prod in x, products))
+
+    data = {
+        'df': df,
+        'companies': companies,
+        'periods': quarters,
+        'products': products
+    }
+
+    return data
 
 
-# df[
-#     (df.delivery_date > '2008-09-09') &
-#     (df.delivery_date <= '2009-09-09') &
-#     (df.company == companies[1])][['company', products[1]]]
-
-prod = '1892ВМ14Я('
-
-product = list(filter(lambda x: '1892ВМ14Я(' in x, products))
-
-for company in companies:
-    for quarter in periods:
-        # print(quarter, f'c {periods[quarter][0]} по {periods[quarter][1]}')
-        if df[(
-                df.delivery_date >= periods[quarter][0]) & (
-                df.delivery_date <= periods[quarter][1]) & (
-                df.company == company)
-                ][product].sum().values.any():
-
-            print(f'Компания - {company}')
-            print(f'за {quarter}')
-            
-            print(df[(
-                df.delivery_date >= periods[quarter][0]) & (
-                df.delivery_date <= periods[quarter][1]) & (
-                df.company == company)][product].sum())
+if __name__ == "__main__":
+    data = init()
+    db_parse(data['df'], data['companies'], data['periods'], data['products'])
 
 
 # for product in products:
