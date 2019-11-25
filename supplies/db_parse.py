e93808e9de6a571b42115ee66a7d602b82d7a7aa
Швецов Сергей Владимирович <shvetsov@elvees.com>
2019-11-25 13:43:14 +0300

    added config file

--- supplies/db_parse.py
+++ supplies/db_parse.py
@@ -1,16 +1,12 @@
+from collections import Counter
+import os
+
 import pandas as pd
 from pandas import DataFrame
 
-from collections import Counter
-
+from db_parse_config import db_file, quarters, products
 
 def db_parse(df, companies, periods, products):
-    # print(df)
-    # print(companies)
-    # print(periods)
-    # print(products)
-
-# result_df = DataFrame({'product':[], 'company':[], 'count':[], 'period':[]})
     results_list = []
 
     for product in products:
@@ -24,8 +20,6 @@ def db_parse(df, companies, periods, products):
 
                 if result_series.values.any():
                     results_list.append([product, company, result_series.sum(), period])
-                    # print(
-                    #     f'{product} - {company} - {result_series.sum()} - {period}')
     
     result = DataFrame(results_list)
 
@@ -42,48 +36,23 @@ def db_parse(df, companies, periods, products):
 
 
 def init():
+    _, ext = os.path.splitext(db_file)
+    if ext in ['.xls', '.xlsx']:
+        df = pd.read_excel(db_file)
+    elif ext in ['.csv']:
+        df = pd.reas_csv(db_file)
+    else:
+        print('Ошибка чтения файла с базой данных!')
+        raise OSError
 
     # Читаем .xlsx файл в DataFrame
-    # df = pd.read_excel('bd.xlsx')
-    df = pd.read_excel('bd.xlsx')
+    
 
     # Заменим все NaN в данных на нули
     df = df.fillna(0)
 
     # Получим массив компаний
     companies = list(Counter(df.company).keys())
-    # print(companies)
-
-    # Предварительный массив продуктов из файла
-    # Подсмотрим и распределим по группам
-
-    # products = list(df.columns[1:-1])
-    # print(products)
-
-    # Продукты по группам
-    # ToDo вынести в конфигурационный файл
-    products = {
-        '1892ВМ15Ф': ['1892ВМ15АФ', '1892ВМ15АФ(ОТК)', '1892ВМ15АФ(ВП)'],
-        '1892BM12Т': ['1892ВМ12АТ', '1892ВМ12АТ (ГК)', '1892BM12Т(ФК НУ)', '1892BM12Т(ОТК)', '1892BM12АТ(ВП)'],
-        '1892ВМ2Я': ['1892ВМ2Т\n("ФК")', '1892ВМ2Я\n(ВП)', '1892ВМ2Т\n(ОТК)', '1892ВМ2Я\n(ОТК)', '1892ВМ2Я\n("ФК")'],
-        '1892ВМ10Я': ['1892ВМ10Я\n(ГК)', '1892ВМ10Я(ФК в НУ)', '1892ВМ10Я\n(ОТК)', '1892ВМ10Я\n(ВП)'],
-    }
-
-    # Отчетный год
-    year = '2019'
-
-    quarters = {
-        f'1кв. {year}г.': [f'{year}-01-01', f'{year}-03-31'],
-        f'2кв. {year}г.': [f'{year}-04-01', f'{year}-06-30'],
-        f'3кв. {year}г.': [f'{year}-07-01', f'{year}-09-30'],
-        f'4кв. {year}г.': [f'{year}-10-01', f'{year}-12-31'],
-    }
-
-    # print(quarters)
-
-    # Подготовка группы товаров, содержащих в prod
-    # prod = 'ча'
-    # product = list(filter(lambda x: prod in x, products))
 
     data = {
         'df': df,
@@ -100,13 +69,3 @@ if __name__ == "__main__":
     db_parse(data['df'], data['companies'], data['periods'], data['products'])
 
 
-# for product in products:
-#     for company in companies:
-#         for i in range(len(periods)-2):
-#             if df[(df.delivery_date > periods[i]) & (df.delivery_date <= periods[i+1]) & (df.company == company)][[product]].sum().values[0]:
-#                 print(f'Компания - {company}')
-#                 print(f'за {periods_kirilic[i+1]}')
-#                 print(df[(df.delivery_date > periods[i]) & (
-#                     df.delivery_date <= periods[i+1]) & (df.company == company)][[product]].sum())
-
-# pd.Series.sum()
