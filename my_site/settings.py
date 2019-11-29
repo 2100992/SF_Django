78d60a0370932393922d6be92050c5f8136ad7fb
Швецов Сергей Владимирович <shvetsov@elvees.com>
2019-11-29 14:48:02 +0300

    connected statics

--- my_site/settings.py
+++ my_site/settings.py
@@ -168,10 +168,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = []
+
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'static'),
 # ]
-
 
 # Что это?
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
