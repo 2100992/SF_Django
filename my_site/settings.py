58a4de942a300f1f3a60eb43625001be939fa4d3
Швецов Сергей Владимирович <shvetsov@elvees.com>
2019-11-29 16:00:18 +0300

    some small fixes

--- my_site/settings.py
+++ my_site/settings.py
@@ -167,6 +167,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+# По этому пути будет доступна статика
+# Например http://mysite.ru/static/p_library
 STATIC_URL = '/static/'
 STATICFILES_DIRS = []
 
