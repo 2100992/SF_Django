b2e775c0067b13ce699571f2c286c206f96b9a37
Shvd <2100992@gmail.com>
2019-12-07 00:24:30 +0300

    cleaned up

--- p_library/urls.py
+++ p_library/urls.py
@@ -1,43 +1,66 @@
 from p_library.models import Book
-from p_library.views import BookDetail, AuthorDetail, PublisherDetail, UserDetail
+from p_library.views import library
+from p_library.views import Books, BookDetail
+from p_library.views import Authors, AuthorDetail
+from p_library.views import Publishers, PublisherDetail
+from p_library.views import UserProfiles, UserDetail
+from p_library.views import Tags, TagDetail
+from p_library.views import BooksCopies, BooksCopyDetail
+from p_library.views import Login, Logout
+from p_library.views import RegisterView, CreateUserProfile
+
 from django.urls import path
-from . import views
+# from . import views
 # from my_site.urls import urlpatterns
 
 app_name = 'p_library'
 
 urlpatterns = [
-    path('', views.library, name='library_url'),
+    path('', library,
+        name='library_url'),
 
-    path('books/', views.Books.as_view(), name='books_list_url'),
-    path('book/<str:slug>/', views.BookDetail.as_view(), name='book_detail_url'),
+    path('books/', Books.as_view(),
+        name='books_list_url'),
+    path('book/<str:slug>/', BookDetail.as_view(),
+        name='book_detail_url'),
 
-    path('book_author/create_many/', views.books_authors_create_many, name='books_authors_create_many'),
+    path('authors/', Authors.as_view(),
+        name='authors_list_url'),
+    path('author/<str:slug>/', AuthorDetail.as_view(),
+        name='author_detail_url'),
 
-    path('authors/', views.Authors.as_view(), name='authors_list_url'),
+    path('publishers/', Publishers.as_view(),
+        name='publishers_list_url'),
+    path('publisher/<str:slug>/', PublisherDetail.as_view(),
+        name='publisher_detail_url'),
 
-    path('author/create/', views.AuthorEdit.as_view(), name='author_create'),
-    path('author/create_many/', views.author_create_many, name='author_create_many'),
+    path('users/', UserProfiles.as_view(),
+        name='users_list_url'),
+    path('user/<str:slug>/', UserDetail.as_view(),
+        name='user_detail_url'),
 
-    path('author/<str:slug>/', views.AuthorDetail.as_view(), name='author_detail_url'),
+    path('tags/', Tags.as_view(),
+        name='tags_list_url'),
+    path('tag/<str:slug>/', TagDetail.as_view(),
+        name='tag_detail_url'),
 
-    path('publishers/', views.Publishers.as_view(), name='publishers_list_url'),
-    path('publisher/<str:slug>/', views.PublisherDetail.as_view(), name='publisher_detail_url'),
+    path('books-copies/', BooksCopies.as_view(),
+        name='copies_list_url'),
+    path('books-copy/<str:uuid>/', BooksCopyDetail.as_view(),
+        name='copy_detail_url'),
 
-    path('f_authors/', views.AuthorList.as_view(), name='authors_list'),
+    path('login/', Login.as_view(),
+        name='login_url'),
+    path('logout/', Logout.as_view(),
+        name='logout_url'),
 
-    path('users/', views.UserProfiles.as_view(), name='users_list_url'),
-    path('user/<str:slug>/', views.UserDetail.as_view(), name='user_detail_url'),
-
-    path('tags/', views.Tags.as_view(), name='tags_list_url'),
-    path('tag/<str:slug>/', views.TagDetail.as_view(), name='tag_detail_url'),
-
-    path('books-copies/', views.BooksCopies.as_view(), name='copies_list_url'),
-    path('books-copy/<str:uuid>/', views.BooksCopyDetail.as_view(), name='copy_detail_url'),
-
-    path('login/', views.Login.as_view(), name='login_url'),
-    path('logout/', views.Logout.as_view(), name='logout_url'),
-
-    path('register-user/', views.RegisterView.as_view(), name='register_user_url'),
-    path('create-user-profile', views.CreateUserProfile.as_view(), name='create_user_profile_url'),
+    path('register-user/', RegisterView.as_view(),
+        name='register_user_url'),
+    path('create-user-profile', CreateUserProfile.as_view(),
+        name='create_user_profile_url'),
 ]
+
+# path('book_author/create_many/', books_authors_create_many, name='books_authors_create_many'),
+# path('author/create/', views.AuthorEdit.as_view(), name='author_create'),
+# path('author/create_many/', views.author_create_many, name='author_create_many'),
+# path('f_authors/', views.AuthorList.as_view(), name='authors_list'),
