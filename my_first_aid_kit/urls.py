46920d92b95dd0827af852c88812105a10d62dbd
Shvd <2100992@gmail.com>
2019-12-11 00:00:33 +0300

    testing GET args

--- my_first_aid_kit/urls.py
+++ my_first_aid_kit/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.HomePageView.as_view(), name='my_first_aid_kit_url'),
     path('medicaments/', views.Medicaments.as_view(), name='medicaments_list_url'),
     path('mykit/', views.MyKit.as_view(), name='my_kit_url'),
+    path('testing/', views.Testing.as_view(), name='testing_url')
     
 ]
--- my_first_aid_kit/views.py
+++ my_first_aid_kit/views.py
@@ -4,7 +4,7 @@ from django.views.generic import ListView, CreateView
 from django.views import View
 from django.urls import reverse_lazy
 
-from .models import *
+from .models import Medicament, Item
 from .forms import MedicamentForm
 
 
@@ -13,19 +13,38 @@ class HomePageView(TemplateView):
     template_name = 'my_first_aid_kit/index.html'
 
 
+class Medicaments(View):
+    template_name = 'my_first_aid_kit/medicaments.html'
+    model = Medicament
+
+    def get(self, request):
+        # success_url = reverse_lazy('my_first_aid_kit:medicaments_list_url')
+        context = {}
+        medicaments = []
+        if request.GET.get('med'):
+            context['med'] = request.GET['med']
+            medicaments = Medicament.objects.filter(slug=context['med']).all()
+        else:
+            medicaments = Medicament.objects.all()
+        context['medicaments'] = medicaments
+        return render(request, self.template_name, context=context)
+
+
+'''
 class Medicaments(ListView):
     model = Medicament
     template_name = 'my_first_aid_kit/medicaments.html'
     success_url = reverse_lazy('my_first_aid_kit:medicaments_list_url')
     context_object_name = 'medicaments'
-    
-    # form_class = MedicamentForm
+'''
 
-    # def get_context_data(self, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #     medicaments = Medicament.objects.all()
-    #     # context['now'] = timezone.now()
-    #     return context
+# form_class = MedicamentForm
+
+# def get_context_data(self, **kwargs):
+#     context = super().get_context_data(**kwargs)
+#     medicaments = Medicament.objects.all()
+#     # context['now'] = timezone.now()
+#     return context
 
 
 class MyKit(ListView):
@@ -33,3 +52,24 @@ class MyKit(ListView):
     content_type = ''
     context_object_name = 'items'
     template_name = 'my_first_aid_kit/items.html'
+
+
+class Testing(View):
+    template_name = 'my_first_aid_kit/testing.html'
+
+    def get(self, request, **kwargs):
+        print()
+        print('ruquest')
+        print(request)
+        print()
+        print('ruquest')
+        print(request.GET['med'])
+        print()
+        print('kwargs')
+        print(kwargs)
+        print()
+        context = {
+            'request': request,
+            'kwargs': kwargs,
+        }
+        return render(request, self.template_name, context=context)
